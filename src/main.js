d3a8d2038d35ac5e2d456ea3870ee4c157f89694
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-07-17 14:25:27 +0200

    "Added navbar import and registration to Alpine data"

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import Alpine from 'alpinejs';
 import homePage from './pages/homePage.js';
 import productDetailViewPage from './pages/productDetailViewPage.js';
 import loginPage from './pages/loginPage.js';
+import navbar from './components/navbar.js';
+
 
 window.Alpine = Alpine;
 
@@ -21,6 +23,7 @@ document.addEventListener('alpine:init', () => {
   Alpine.data('homePage', homePage);
   Alpine.data('productDetailViewPage', productDetailViewPage);
   Alpine.data('loginPage', loginPage);
+  Alpine.data('navbar', navbar);
 });
 
 Alpine.start();
