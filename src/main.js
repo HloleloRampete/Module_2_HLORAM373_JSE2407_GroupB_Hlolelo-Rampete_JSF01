826f0c010cf683dad737ec379f2d2a0e9f675742
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-07-17 13:42:54 +0200

    "Initialized the html structure, created a styling file and a main javascript file"

---
+++ src/main.js
@@ -0,0 +1,26 @@
+// src/main.js
+import Alpine from 'alpinejs';
+import homePage from './pages/homePage.js';
+import productDetailViewPage from './pages/productDetailViewPage.js';
+import loginPage from './pages/loginPage.js';
+
+window.Alpine = Alpine;
+
+document.addEventListener('alpine:init', () => {
+  Alpine.store('app', {
+    currentPage: 'home',
+    productId: null,
+  });
+
+  Alpine.data('app', () => ({
+    init() {
+      // Any initialization logic can go here
+    },
+  }));
+
+  Alpine.data('homePage', homePage);
+  Alpine.data('productDetailViewPage', productDetailViewPage);
+  Alpine.data('loginPage', loginPage);
+});
+
+Alpine.start();
